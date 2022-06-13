035179b01e2c4790ebe9faa4c70b3961e1332ec0
Jonathan Cockrem <jctotalportal@gmail.com>
2022-06-12 23:05:19 -0400

    update script

--- build/setup_actions.sh
+++ build/setup_actions.sh
@@ -4,8 +4,8 @@ echo "$COMMON_KEY" > ~/.ssh/id_common
 chmod 600 ~/.ssh/id_common
 printf "Host githbut.com \n\t Hostname github.com \n\t StrictHostKeyChecking no \n\t User git \n\t IdentityFile ~/.ssh/id_common" >> ~/.ssh/config
 
-git config --global user.email "robot.condor@johnnyemail.com"
-git config --global user.name "robotcondor"
+git config --global user.email "jctotalportal@gmail.com"
+git config --global user.name "Jonathan Cockrem"
 git config --global url."ssh://gi@github.com/".insteadOF "https//github.com/"
 
 eval $(ssh-agent -s)
